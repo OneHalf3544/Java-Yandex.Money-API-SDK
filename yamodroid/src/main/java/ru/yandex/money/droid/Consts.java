595700c9227c5225f82da4c4299714c4d4cafce5
OneHalf_3544 <onehalf354@gmail.com>
2014-05-26 22:44:37 +0400

    Удалил yamodroid

--- yamodroid/src/main/java/ru/yandex/money/droid/Consts.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.money.droid;
-
-class Consts {      
-        
-    public static final String WAIT = "Пожалуйста, подождите";
-    
-    public static String USER_AGENT = "yamodroid";    
-}
